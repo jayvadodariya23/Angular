de829f841d3010b7652f5ec3ee921fab4ec0cc4f
Jay <jayvadodariya20@gmail.com>
2022-05-10 11:13:13 +0530

    first commit

---
+++ angular-practice/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Hello World</h1>`,
+  styles: [`
+  h1{
+    color : red;
+  }
+  `]
+})
+export class AppComponent {
+}
